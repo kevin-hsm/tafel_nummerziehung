ec94879f27bc89aa3bfc6af08cfabdb2e24c4789
kevin-hsm <kevin.hirschmayer@icloud.com>
2025-11-16 17:13:54 +0100

    server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from threading import Lock
+import os
 
 app = Flask(__name__)
 
@@ -62,4 +63,6 @@ def next_customer():
 # Starten
 # -------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
